aa661625f457db283b2dc6732170d532f100bfbc
lunov <a3rov@yandex.ru>
2023-11-19 00:30:40 +0300

    First

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
+from random import randint
+from PyQt5.QtCore import Qt
+
+
+class Application(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.draw = False
+        uic.loadUi('Ui.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.do)
+
+        self.label = QLabel()
+        canvas = QPixmap(600, 600)
+        self.label.setPixmap(canvas)
+
+        layout = QGridLayout(self.centralwidget)
+        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
+        layout.addWidget(self.label, 1, 0)
+
+    def do(self):
+        x, y = [randint(10, 500) for i in range(2)]
+        w = h = randint(10, 100)
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor(*[randint(0, 255) for i in range(3)]))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, w, h)
+        painter.end()
+        self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Application()
+    ex.show()
+    sys.exit(app.exec_())
